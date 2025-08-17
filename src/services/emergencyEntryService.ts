466f4fe40a896140134066bd85a577d7d5dfee61
Seu Nome <jhessika.smp@gmail.com>
2025-08-17 12:27:55 +0200

    Correcao erro do vercel erro 8

--- src/services/emergencyEntryService.ts
+++ src/services/emergencyEntryService.ts
@@ -1,5 +1,5 @@
 export async function getEmergencyEntriesYear(userId: string, year: number) {
-  const res = await fetch(`http://localhost:5000/api/emergency-entry/year/${userId}/${year}`);
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/emergency-entry/year/${userId}/${year}`);
   const data = await res.json();
   return data.success ? data.data : [];
 }
