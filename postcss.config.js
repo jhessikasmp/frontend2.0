3dbc471f9e43c5072d851ea7d2f63b49f99068c7
Seu Nome <jhessika.smp@gmail.com>
2025-08-17 08:40:07 +0200

    erro do css cocertado

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// This file must use CommonJS syntax because Vite treats it as a config file, not a module.
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-};
