ed4e003336c533d01f245af145273df05bf5cb22
Seu Nome <jhessika.smp@gmail.com>
2025-08-17 17:35:16 +0200

    Correcao da pagina de Relatorio anul

--- src/services/getAnnualExpenses.ts
+++ src/services/getAnnualExpenses.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export async function getAnnualExpenses(userId: string, year: number) {
-  const res = await axios.get(`/api/expense/user/${userId}/year/${year}`);
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const res = await axios.get(`${apiUrl}/api/expense/user/${userId}/year/${year}`);
   return res.data.data || [];
 }
