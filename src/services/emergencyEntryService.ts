28c4765234a467ee8831eaf6af6156ffeb3dd491
Seu Nome <jhessika.smp@gmail.com>
2025-08-20 09:47:01 +0200

    Corrigindo erro da pagina emergencia 2

--- src/services/emergencyEntryService.ts
+++ src/services/emergencyEntryService.ts
@@ -1,5 +1,11 @@
 export async function getEmergencyEntriesYear(userId: string, year: number) {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/emergency-entry/year/${userId}/${year}`);
+  let url;
+  if (!userId) {
+    url = `${import.meta.env.VITE_API_URL}/api/emergency-entry/year/${year}`;
+  } else {
+    url = `${import.meta.env.VITE_API_URL}/api/emergency-entry/year/${userId}/${year}`;
+  }
+  const res = await fetch(url);
   const data = await res.json();
   return data.success ? data.data : [];
 }
