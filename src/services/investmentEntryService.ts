1970dd742eb2f8eeb43cd41b6d4841b2b61e7e87
Seu Nome <jhessika.smp@gmail.com>
2025-08-20 09:14:54 +0200

    Corrigindo erro da pagina investimento

--- src/services/investmentEntryService.ts
+++ src/services/investmentEntryService.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export async function getInvestmentEntriesYear(userId: string, year: number) {
   const apiUrl = import.meta.env.VITE_API_URL;
-  const res = await axios.get(`${apiUrl}/api/investment-entry/user/${userId}`);
-  // Filtra apenas as entradas do ano atual
-  return res.data.data.filter((entry: any) => new Date(entry.date).getFullYear() === year);
+  const res = await axios.get(`${apiUrl}/api/investment-entry/year/${year}`);
+  return res.data.data;
 }
