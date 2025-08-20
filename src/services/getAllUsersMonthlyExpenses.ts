ef862778b07d96cc562336c52db35a4f5fb5e9b9
Seu Nome <jhessika.smp@gmail.com>
2025-08-20 11:49:03 +0200

    Corrigindo alguns erros 2

--- src/services/getAllUsersMonthlyExpenses.ts
+++ src/services/getAllUsersMonthlyExpenses.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export async function getAllUsersMonthlyExpenses() {
-  const res = await axios.get('/api/expense/monthly-total');
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const res = await axios.get(`${apiUrl}/api/expense/monthly-total`);
   return res.data.data || [];
 }
